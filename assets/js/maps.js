25abb42598d47cdfe4abe62010cf9e67f0a4fcfd
MariaLundgren <melundgren93@gmail.com>
2021-04-07 14:11:47 +0000

    Added functionality to buttons in locations section to switch between the different cities on google maps

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,6 +1,7 @@
 // this section is made from Eamonn Smyths document "How to google maps" found on code institutes slack channel and then I have added my own code to make it sutible for my project
-function myMap() {
-    const mapLocation = {
+
+function initMap() {
+    var mapLocation = {
         zoom: 15,
         center: {
             lat: 59.33179, 
@@ -8,18 +9,37 @@ function myMap() {
         }
         };
 
-    const map = new google.maps.Map(document.getElementById("map"), mapLocation);
+    var map = new google.maps.Map(document.getElementById("map"), mapLocation);
 
-    const myMarkers = [
+    myMarkers = [
         {lat: 59.33179, lng:18.04498},
         {lat: 57.70338, lng:11.97268}, 
-        {lat: 55.60681, lng: 13.00025}, 
+        {lat: 55.60681, lng: 13.00025},
     ];
 
     for(let i=0; i < myMarkers.length; i++) {
         const markers = new google.maps.Marker({position: myMarkers[i]}); 
         markers.setMap(map);
     }
+
+    // change to the different cities when pressing the buttons
+    const goteborgMap = document.getElementById("goteborgMap")
+
+    goteborgMap.addEventListener("click", function(event) {
+        map.setCenter(new google.maps.LatLng(57.70338, 11.97268));
+    });
+
+    const malmoMap = document.getElementById("malmoMap")
+
+    malmoMap.addEventListener("click", function(event) {
+        map.setCenter(new google.maps.LatLng(55.60681, 13.00025));
+    });
+
+    const stockholmMap = document.getElementById("stockholmMap")
+
+    stockholmMap.addEventListener("click", function(event) {
+        map.setCenter(new google.maps.LatLng(59.33179, 18.04498));
+    });
 }
 
-myMap()
+initMap()
