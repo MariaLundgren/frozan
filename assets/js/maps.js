6397de2140c846ab2cd6e2ab2f0e110b904436d5
MariaLundgren <melundgren93@gmail.com>
2021-04-07 08:33:48 +0000

    finished implementing google maps api with markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,26 +1,25 @@
-// this section is made with help from code instituts video lessons on google maps api and Eamonn Smyths document "How to google maps" found on code institutes slack channel
-function initMap() {
-            const map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 15, 
-                center: {
-                    lat: 59.33179,
-                    lng: 18.04498
-                }
-            });
-
-            const myMarkers = [
-                {lat: 59.33179, lng:18.04498},
-                {lat: 57.70338, lng:11.97268}, 
-                {lat: 55.60681, lng: 13.00025}, 
-            ];
-
-            const markers = myMarkers.map(function(myMarkers, i) {
-                return new google.maps.Marker({
-                    position:myMarkers,
-                });
-            });
-
-            new markerCluster = new MarkerCluster(map, markers, {
-                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-            });
+// this section is made from Eamonn Smyths document "How to google maps" found on code institutes slack channel and then I have added my own code to make it sutible for my project
+function myMap() {
+    const mapLocation = {
+        zoom: 15,
+        center: {
+            lat: 59.33179, 
+            lng: 18.04498
         }
+        };
+
+    const map = new google.maps.Map(document.getElementById("map"), mapLocation);
+
+    const myMarkers = [
+        {lat: 59.33179, lng:18.04498},
+        {lat: 57.70338, lng:11.97268}, 
+        {lat: 55.60681, lng: 13.00025}, 
+    ];
+
+    for(let i=0; i < myMarkers.length; i++) {
+        const markers = new google.maps.Marker({position: myMarkers[i]}); 
+        markers.setMap(map);
+    }
+}
+
+myMap()
