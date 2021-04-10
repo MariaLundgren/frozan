8ecc5ff0c7c963de63feb9069a2b49e1f55853be
MariaLundgren <melundgren93@gmail.com>
2021-04-10 16:13:15 +0000

    added animation to textin about us section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
 //get the elements I want to add animations to
 const iceCreamText = document.querySelector(".iceCreamText");
+const aboutText = document.querySelector(".aboutText");
+
 
 
 
 //function to animate the text
 function fadeIn(){
     //get the position of iceCreamText on the y-axis of the page
-    let textPosition = iceCreamText.getBoundingClientRect().y;
+    let textPosition1 = aboutText.getBoundingClientRect().y;
+    let textPosition2 = iceCreamText.getBoundingClientRect().y;
     // set how far we scrolled on page 
-    let scrolled = window.pageYOffset;
+    let scrolled = window.pageYOffset / 1.2;
     // if scrolled is larger or equal to text position add active class
-    if (scrolled >= textPosition) {
+    if (scrolled >= textPosition1) {
+        aboutText.classList.add("animation");
+    }
+    if (scrolled >= textPosition2) {
         iceCreamText.classList.add("animation");
     }
 };
