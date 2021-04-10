dc9d4af93625b64020eab9ff29302d2e882f5768
MariaLundgren <melundgren93@gmail.com>
2021-04-10 12:15:08 +0000

    Continued working on code to animate text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 //get the elements I want to add animations to
 const iceCreamText = document.getElementById("iceCreamText");
-const aboutText = document.getElementById("aboutText");
 
 //function to animate the text
-function fadeIn() {
+function fadeIn(){
     //get the position of iceCreamText on the y-axis of the page
     let textPosition = iceCreamText.getBoundingClientRect().y;
+    // set how far we scrolled on page 
+    let scrolled = window.pageYOffset;
+    // if scrolled is larger or equal to text position add active class
+    if (scrolled >= textPosition) {
+        iceCreamText.classList.add("active");
+    }
 };
 
-//call the function
-fadeIn();
+
+window.addEventListener("scroll",fadeIn);
+
