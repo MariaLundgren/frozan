f1a3b81f7bb2259534b3b48833a392ed2964e961
MariaLundgren <melundgren93@gmail.com>
2021-04-12 08:02:41 +0000

    removed consts in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,18 @@
 
-//get the elements I want to add animations to
-const iceCreamText = document.querySelector(".iceCreamText");
-const aboutText = document.querySelector(".aboutText");
-
 //function to animate the text
 function fadeIn(){
     //get the position of iceCreamText on the y-axis of the page
-    let textPosition1 = aboutText.getBoundingClientRect().y;
-    let textPosition2 = iceCreamText.getBoundingClientRect().y;
+    let textPosition1 = document.querySelector(".aboutText").getBoundingClientRect().y;
+    let textPosition2 = document.querySelector(".iceCreamText").getBoundingClientRect().y;
     // set how far we scrolled on page 
     let scrolled = window.pageYOffset;
     // if scrolled is larger or equal to text position add active class
     if (scrolled >= textPosition1) {
-        aboutText.classList.add("animation");
+        document.querySelector(".aboutText").classList.add("animation");
     }
     if (scrolled >= textPosition2) {
-        iceCreamText.classList.add("animation");
+        document.querySelector(".iceCreamText").classList.add("animation");
     }
 };
 
-window.addEventListener("scroll",fadeIn)
+    window.addEventListener("scroll",fadeIn)
