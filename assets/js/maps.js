f1fb4ab0e01758f0f84468e44b1d7ac01e854e86
MariaLundgren <melundgren93@gmail.com>
2021-04-12 07:35:29 +0000

    moved consts out of the initMap funtion in maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,38 +1,49 @@
 // this section down to the buttons that change locations is made from Cone Institutes video lessons on Google Maps API and 
 // Eamonn Smyths document "How to google maps" found on code institutes slack channel and custonized it to make it suit my project
 
+const mapLocation = {
+    zoom: 15,
+    center: {
+        lat: 59.33179, 
+        lng: 18.04498
+    }
+    };
+
+const myMarkers = [
+    {
+        lat: 59.33179, lng: 18.04498,
+        "title": "frozan Stockholm",
+        "adress": "Scheelegatan 24, 112 28 Stockholm",
+    },
+    {
+        lat: 57.70338, lng: 11.97268,
+        "title": "frosan Göteborg",
+        "adress": "Gamla Allén 2, 411 06 Göteborg"
+    },
+    {
+        lat: 55.60681, lng: 13.00025,
+        "title": "frozan Malmö",
+        "adress":"Stortorget 3, 211 22 Malmö"
+    }
+];
+
+const icon = "assets/images/marker.png"
+
+const goteborgMap = document.getElementById("goteborgMap");
+
+const malmoMap = document.getElementById("malmoMap");
+
+const stockholmMap = document.getElementById("stockholmMap");
+
+
+
+
+
 function initMap() {
     window.onload = (event) => {
-    const mapLocation = {
-        zoom: 15,
-        center: {
-            lat: 59.33179, 
-            lng: 18.04498
-        }
-        };
 
-    const map = new google.maps.Map(document.getElementById("map"), mapLocation);
+        const map = new google.maps.Map(document.getElementById("map"), mapLocation);
     
-    const myMarkers = [
-        {
-            lat: 59.33179, lng: 18.04498,
-            "title": "frozan Stockholm",
-            "adress": "Scheelegatan 24, 112 28 Stockholm",
-        },
-        {
-            lat: 57.70338, lng: 11.97268,
-            "title": "frosan Göteborg",
-            "adress": "Gamla Allén 2, 411 06 Göteborg"
-        },
-        {
-            lat: 55.60681, lng: 13.00025,
-            "title": "frozan Malmö",
-            "adress":"Stortorget 3, 211 22 Malmö"
-        }
-    ];
-
-    const icon = "assets/images/marker.png"
-
     for(let i=0; i < myMarkers.length; i++) {
         const markers = new google.maps.Marker({
         position: new google.maps.LatLng(myMarkers[i], myMarkers[i]),
@@ -51,24 +62,17 @@ function initMap() {
     }
 
     // change to the different cities when pressing the buttons
-    const goteborgMap = document.getElementById("goteborgMap");
 
     goteborgMap.addEventListener("click", function(event) {
         map.setCenter(new google.maps.LatLng(57.70338, 11.97268));
     });
 
-    const malmoMap = document.getElementById("malmoMap");
-
     malmoMap.addEventListener("click", function(event) {
         map.setCenter(new google.maps.LatLng(55.60681, 13.00025));
     });
-
-    const stockholmMap = document.getElementById("stockholmMap");
 
     stockholmMap.addEventListener("click", function(event) {
         map.setCenter(new google.maps.LatLng(59.33179, 18.04498));
     });
 };
 }
-
-initMap();
