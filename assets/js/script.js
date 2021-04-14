dde5bd99596186ee2f98ff2bbdf4fbc228f1ef14
MariaLundgren <melundgren93@gmail.com>
2021-04-14 13:51:00 +0000

    changed comments in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
-
-//function to animate the text
+/**
+ * Adds the fade in effect when scrolling down the page.
+ * @param {}
+ * @returns {} 
+ */
 function fadeIn(){
-    //get the position of iceCreamText on the y-axis of the page
+  
     let textPosition1 = document.querySelector(".aboutText").getBoundingClientRect().y;
     let textPosition2 = document.querySelector(".iceCreamText").getBoundingClientRect().y;
-    // set how far we scrolled on page 
+
     let scrolled = window.pageYOffset;
-    // if scrolled is larger or equal to text position add active class
+
     if (scrolled >= textPosition1) {
         document.querySelector(".aboutText").classList.add("animation");
     }
+
     if (scrolled >= textPosition2) {
         document.querySelector(".iceCreamText").classList.add("animation");
     }
@@ -18,5 +22,5 @@ function fadeIn(){
 
 
 window.addEventListener("DOMContentLoaded", (event) => {
-window.addEventListener("scroll",fadeIn)
+    window.addEventListener("scroll",fadeIn)
 });
