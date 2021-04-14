8db428428d70d9a523d6623ee7576ba4afd116db
MariaLundgren <melundgren93@gmail.com>
2021-04-14 10:01:58 +0000

    Added error text that shows in modal if an error occurs when submitting form

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,15 +1,16 @@
 const MODAL = document.getElementById("modal-background");
+const MODAL_TEXT = document.getElementById("modalText");
 
-function showSuccessMessage() {
+function showModal() {
     MODAL.style.display = "block";
 }
 
 document.getElementById("modal-button").addEventListener("click", function() {
     MODAL.style.display = "none";
-})
+});
 
 // section taken from code institute lessons on emailj js and customized to my project 
-function sendMail(contactForm) {
+function sendMail(contactForm) { 
     emailjs.send("service_nj2gh0e", "frozan", {
         "from_name":contactForm.name.value,
         "from_email": contactForm.email.value,
@@ -19,10 +20,13 @@ function sendMail(contactForm) {
         function(response) {
             console.log("success", response);
             document.getElementById("myForm").reset();
-            showSuccessMessage();
+            showModal();
+            MODAL_TEXT.innerHTML = "Thank you for your message! <br> We usually respond within 48 hours on weekdays.";
         }, 
         function(error) {
             console.log("failed", error);
+            showModal();
+            MODAL_TEXT.innerHTML = "Something went wrong! Please try again later.";
         });
         return false;
 }
