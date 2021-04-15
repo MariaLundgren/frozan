52474f66ba730ca2b2d26001e2d8d099d6079366
MariaLundgren <melundgren93@gmail.com>
2021-04-15 17:36:39 +0000

    removed domContentLoaded from email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,9 +2,7 @@
  * Shows modal when submitting contact form and adds click function to modal button to be able to close modal.
  */
 function showModal() {
-  window.addEventListener("DOMContentLoaded", (event) => {
   document.getElementById("modal-background").style.display = "block";
-  });
 }
 
 document.getElementById("modal-button").addEventListener("click", function () {
